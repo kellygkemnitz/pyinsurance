4ba60b07e03e940a32cc84ea90351100b2cdbc6b
Kelly Kemnitz <kellygkemnitz@gmail.com>
2024-10-10 20:25:08 -0500

    Creating Dash and Streamlit apps

--- pyinsurance.py
+++ pyinsurance.py
@@ -1,12 +1,7 @@
-import dash
-from dash import dcc, html
-import dash_bootstrap_components as dbc
-from dash.dependencies import Output, Input
+import openpyxl
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
-
 
 
 class Insurance:
@@ -143,33 +138,3 @@ class Insurance:
 
         return auto_figure
     
-
-insurance = Insurance()
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.ZEPHYR, dbc.icons.FONT_AWESOME])
-
-app.layout = html.Div([
-    dbc.Tabs(
-        id='tabs',
-        children=[
-            dbc.Tab(label='Home Insurance', tab_id='tab-1', children=[
-                dcc.Graph(
-                    id='home-insurance-plots',
-                    figure=insurance.create_home_insurance_plots,
-                    config={'displayModeBar': False},
-                )
-            ]),
-            dbc.Tab(label='Auto Insurance', tab_id='tab-2', children=[
-                dcc.Graph(
-                    id='auto-insurance-plots',
-                    figure=insurance.create_auto_insurance_plots,
-                    config={'displayModeBar': False},
-                )
-            ])
-        ]
-    )
-])
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
