6d98b876a2745f411d95b1e6cf316f93565cac20
Kelly Kemnitz <kellygkemnitz@gmail.com>
2025-02-17 20:24:17 -0600

    Committing work for tonight

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
+import os
+import sys
+
 import dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
+
+# Manually set the parent directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from modules.pyinsurance import Insurance
 
